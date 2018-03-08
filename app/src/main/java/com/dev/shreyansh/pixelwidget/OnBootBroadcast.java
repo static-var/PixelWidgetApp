9388471915badc2b26262cce7a483851ef2e8800
Shreyansh Lodha <slodha96@gmail.com>
2018-03-08 23:41:18 +0530

    Add OnBootBroadcast class to schedule jobs again when phone restarts
    Signed-off-by: Shreyansh Lodha <slodha96@gmail.com>

---
+++ app/src/main/java/com/dev/shreyansh/pixelwidget/OnBootBroadcast.java
@@ -0,0 +1,22 @@
+package com.dev.shreyansh.pixelwidget;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by shreyansh on 3/8/18.
+ */
+
+public class OnBootBroadcast extends BroadcastReceiver {
+
+    public static final String TAG = OnBootBroadcast.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG,"Broadcast receiver for Pixel Weather App.");
+        /* Schedule Jobs here */
+    }
+}
