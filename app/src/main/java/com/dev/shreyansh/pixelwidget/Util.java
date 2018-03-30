9845f2c50f7c0a2ed7ccbab4de5af56fa0751efd
Shreyansh Lodha <slodha96@gmail.com>
2018-03-30 14:02:47 +0530

    Util.java : Add checkNetwork and pingGoogle functions in this class

--- app/src/main/java/com/dev/shreyansh/pixelwidget/Util.java
+++ app/src/main/java/com/dev/shreyansh/pixelwidget/Util.java
@@ -4,6 +4,8 @@ import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.PersistableBundle;
 import android.util.Log;
@@ -16,7 +18,7 @@ public class Util {
     private static final String TAG = Util.class.getSimpleName();
 
     private static final int JOB_WIDGET_UPDATE = 1001;
-    private static final int REFRESH_INTERVAL = 15 * 60 * 1000;
+    private static final int REFRESH_INTERVAL = 5 * 1000;
 
     public static void widgetData(Context context) {
 
@@ -26,14 +28,40 @@ public class Util {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             builder.setMinimumLatency(REFRESH_INTERVAL);
-            builder.setOverrideDeadline(REFRESH_INTERVAL);
+                builder.setOverrideDeadline(REFRESH_INTERVAL);
         } else {
             builder.setPeriodic(REFRESH_INTERVAL);
         }
         builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
         builder.setRequiresCharging(false); // we don't care if the device is charging or not
+
         JobScheduler jobScheduler = (JobScheduler)  context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
         jobScheduler.schedule(builder.build());
+    }
+
+    public static boolean checkNetwork(Context context) {
+        /* Check the network status */
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+            //should check null because in airplane mode it will be null
+            return (netInfo != null && netInfo.isConnected());
+        }
+        catch (Exception e) {
+            Log.e(TAG, "Error", e);
+            return false;
+        }
+    }
+
+    public static boolean pingGoogle() {
+        try {
+            String command = "ping -c 1 google.com";
+            return (Runtime.getRuntime().exec(command).waitFor() == 0);
+        } catch (Exception e) {
+            Log.i(TAG, "Error", e);
+            return false;
+        }
     }
 
 }
