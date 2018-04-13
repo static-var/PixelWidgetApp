ffda1c055465753f5a12523dd72962283512775c
Shreyansh Lodha <slodha96@gmail.com>
2018-04-13 14:38:30 +0530

    add Settings Activity

---
+++ app/src/main/java/com/dev/shreyansh/pixelwidget/SettingsActivity.java
@@ -0,0 +1,43 @@
+package com.dev.shreyansh.pixelwidget;
+
+import android.R.*;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class SettingsActivity extends AppCompatActivity {
+    private SharedPreferences sp;
+    private SharedPreferences.Editor spe;
+
+    private Switch calendarApp;
+    private Switch calendarOnline;
+    private Switch unit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+        sp = getSharedPreferences("PIXEL", MODE_PRIVATE);
+
+        this.getSupportActionBar().setElevation(0);
+        this.getSupportActionBar().setTitle("Settings");
+
+
+        bindUI();
+
+        checkedChangedListener();
+    }
+
+    private void bindUI() {
+
+    }
+
+    private void checkedChangedListener() {
+    }
+}
