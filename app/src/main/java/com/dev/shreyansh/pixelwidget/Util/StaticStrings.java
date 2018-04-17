4bf35d356c6fc1f5db7c4ffa761cbfaeb7b4f5e8
Shreyansh Lodha <slodha96@gmail.com>
2018-04-17 16:24:34 +0530

    StaticString : Move all constant strings in 1 class

---
+++ app/src/main/java/com/dev/shreyansh/pixelwidget/Util/StaticStrings.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2017-2018 Shreyansh Lodha <slodha96@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PixelWidget.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.dev.shreyansh.pixelwidget.Util;
+
+public class StaticStrings {
+    public static final String SP = "PIXEL";
+    public static final String SHOW_CITY = "SHOW_CITY";
+    public static final String UNIT_F = "UNIT_F";
+    public static final String CAL_APP = "CAL_APP";
+    public static final String REMINDER_IMP = "REMINDER_IMP";
+
+    public static final String CENTIGRADE = " " + (char) 0x00B0 + "C";
+    public static final String FARENHEIT = " " + (char) 0x00B0 + "F";
+
+    public static final String CALENDAR_APP = "com.google.android.calendar";
+}
